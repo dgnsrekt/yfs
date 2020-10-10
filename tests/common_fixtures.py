196d15b73ca06f00d176302c2c6bba390fe72912
dgnsrekt <dgnsrekt@pm.me>
2020-10-09 21:57:06 -0500

    test: Added more raw test data, statistics tests, and summary tests.

--- tests/common_fixtures.py
+++ tests/common_fixtures.py
@@ -1,3 +1,4 @@
+from itertools import permutations
 from pathlib import Path
 
 import pytest
@@ -14,13 +15,65 @@ def get_data(path: Path) -> HTML:
         return HTML(html=file.read())
 
 
-@pytest.fixture(params=["aapl"])
+@pytest.fixture(
+    params=[
+        "aapl",
+        "amd",
+        "amzn",
+        "dia",
+        "exfo",
+        "fcel",
+        "gpro",
+        "lite",
+        "msft",
+        "pavm",
+        "tsla",
+        "twtr",
+    ]
+)
 def summary_page_data_fixture(request):
     symbol = request.param
-    test_response_path = TEST_DIRECTORY / "data" / f"{symbol}_summary_page_raw.html"
+    test_response_path = TEST_DIRECTORY / "data" / "summary" / f"{symbol}_summary_page_raw.html"
     return symbol, get_data(test_response_path)
 
 
+symbols = [
+    "aapl",
+    "amd",
+    "amzn",
+    "dia",
+    "exfo",
+    "fcel",
+    "twtr",
+]
+input = list(permutations(symbols, r=2))
+output = [sorted(c) for c in input]
+
+
+@pytest.fixture(params=list(zip(input, output)))
+def multiple_summary_page_data_fixture(request):
+    from pprint import pprint
+
+    input, output = request.param
+    symbol_one, symbol_two = input
+
+    test_response_path_one = (
+        TEST_DIRECTORY / "data" / "summary" / f"{symbol_one}_summary_page_raw.html"
+    )
+    test_response_path_two = (
+        TEST_DIRECTORY / "data" / "summary" / f"{symbol_two}_summary_page_raw.html"
+    )
+    output = [symbol.upper() for symbol in output]
+    return (
+        symbol_one,
+        symbol_two,
+        get_data(test_response_path_one),
+        get_data(test_response_path_two),
+        output,
+    )
+
+
+#
 @pytest.fixture
 def option_expiration_data_fixture():
     test_response_path = TEST_DIRECTORY / "data" / "spy_option_expiration_raw.html"
--- tests/test_summary.py
+++ tests/test_summary.py
@@ -3,16 +3,13 @@ from pytest_regressions import data_regression  # noqa: F401
 
 from collections import ChainMap
 
-from yfs.summary import parse_summary_table, SummaryPage
+from yfs.summary import parse_summary_table, SummaryPage, SummaryPageGroup
 from yfs.quote import parse_quote_header_info
 
-from .common_fixtures import summary_page_data_fixture
+from .common_fixtures import summary_page_data_fixture, multiple_summary_page_data_fixture
 
 
-def test_parse_summary_table(data_regression, summary_page_data_fixture):
-
-    symbol, data = summary_page_data_fixture
-
+def create_summary_page_object(symbol, data):
     summary = parse_summary_table(data)
     quote = parse_quote_header_info(data)
 
@@ -20,6 +17,28 @@ def test_parse_summary_table(data_regression, summary_page_data_fixture):
     data["symbol"] = symbol
     data["quote"] = quote
 
-    result = SummaryPage(**data)
+    return SummaryPage(**data)
+
+
+def test_parse_summary_table(data_regression, summary_page_data_fixture):
+
+    symbol, data = summary_page_data_fixture
+
+    result = create_summary_page_object(symbol, data)
+
+    assert result.high > result.low
 
     data_regression.check(result.json())
+
+
+def test_sorting_summary_pages(multiple_summary_page_data_fixture):
+    symbol_one, symbol_two, data_one, data_two, target = multiple_summary_page_data_fixture
+    page_one = create_summary_page_object(symbol_one, data_one)
+    page_two = create_summary_page_object(symbol_two, data_two)
+
+    summary_page_group = SummaryPageGroup()
+
+    summary_page_group.append(page_one)
+    summary_page_group.append(page_two)
+    summary_page_group.sort()
+    assert summary_page_group.symbols == target
